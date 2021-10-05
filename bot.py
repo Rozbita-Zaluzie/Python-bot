2bd8187f4d7ee79412a0f17f4206da3b772ad267
Rozbita-Zaluzie <64742692+Rozbita-Zaluzie@users.noreply.github.com>
2021-10-05 21:04:02 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,69 @@
+#import kni-hoven or what
+import discord
+import datetime
+from discord.ext import commands, tasks
+from discord import message, guild, Guild, channel
+import random
+import os
+from os import system
+from discord.utils import get
+import time
+
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix = '.', intents=intents)
+
+
+@commands.has_permissions(view_channel = True)
+
+#@client.event
+#async def on_ready():
+
+
+#on_ready
+@client.event
+async def on_ready():
+    print("jsem připravenej ti ojet mámu debile")
+
+#! .find
+@client.command()
+async def find(ctx):
+
+
+    channel = client.get_channel(775322766814740512)
+
+    print(channel.name)
+
+    category = channel.category
+    channels = category.channels
+    for x in channels:
+        await ctx.send(x)
+
+
+#! .join
+@client.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
+
+   
+#! .leave
+@client.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+
+
+#! .members
+@client.command()
+async def members(ctx):
+    await ctx.send(f"{ctx.guild.member_count} members.")
+    for x in ctx.guild.members:
+        await ctx.send(x)
+
+#* .howareu
+@client.command()
+@commands.has_any_role("Majitel", "Admin")
+async def howareu(ctx):
+    await ctx.send("Mám se fajne", "Jde to", "Bylo líp", "mega fajn!", "ja neviem už :D")
+
+
+client.run("ODk0OTgzMDY4MjExOTAwNDg3.YVx77w.kkHHUwBpApEW-PmE08-6FDhO6Ro")
---
+++ readme.md
@@ -0,0 +1,28 @@
+Python-bot
+Version: 0.01
+
+Welcome to our github page where you can find a bot named Daisuke, it was created for school purposes and mostly for finding certain data on discord servers for easier learning.
+
+------------------------------------------------------------
+
+Command Prefix: "."
+
+Commands:
+    1. .find 
+    2. .join (join bot to voice channel)
+    3. .leave (leave bot from voice channel)
+    4. .members (write down members from server)
+    5. coming soon!
+
+
+
+
+
+
+------------------------------------------------------------
+
+HOW TO USE:
+1. open cmd
+2. run bot.py
+3. enjoy!
+------------------------------------------------------------
